6ec3c63b7508b072d79d2102e06d4517b85fc5d8
DabGias <gabrielfurlaneti@gmail.com>
2023-08-15 17:37:27 -0300

    commit inicial

--- src/app/add-alimento/page.jsx
+++ src/app/add-alimento/page.jsx
@@ -1,10 +1,15 @@
-import { Header } from "@/components/Header";
+import { Button } from "@/components/Button"
+import { Header } from "@/components/Header"
 
 export default function FormAddAlimento() {
     return(
         <>
             <Header/>
             <h1 className="text-center">Adicionar alimento</h1>
+
+            <div className="flex justify-center">
+                <Button path={"/"} text={"+ Adicionar porção/alimento"}/>
+            </div>
         </>
     )
 }
--- src/app/add-refeicao/page.jsx
+++ src/app/add-refeicao/page.jsx
@@ -1,10 +1,15 @@
-import { Header } from "@/components/Header";
+import { Button } from "@/components/Button"
+import { Header } from "@/components/Header"
 
 export default function FormAddRefeicao() {
     return(
         <>
             <Header/>
             <h1 className="text-center">Adicionar refeição</h1>
+
+            <div className="flex justify-center">
+                <Button path={"/"} text={"+ Adicionar refeição"}/>
+            </div>
         </>
     )
 }
